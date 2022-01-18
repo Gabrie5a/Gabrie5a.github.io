52cdf383584fd471169e5090286cd3c250ff7a1c
Gabrie5a <80426217+Gabrie5a@users.noreply.github.com>
2022-01-17 23:14:02 -0500

    Project 2 + week 3

---
+++ Everup/week3/day1.js
@@ -0,0 +1,66 @@
+console.log(2+2);
+let cup = 'coffe';
+if (cup === 'coffee') alert('Amazing!');
+let ppl = document.querySelector('div');
+addEventListener
+let yearjohn = 1996;
+let ageJohn = 24;
+//camel
+const myBestFriend = 'John';
+let yearJohn = 1998;
+let _dollar;
+let $john;
+const PI = 3.14;
+//string uses ""
+//can use + or ,
+console.log("Let's go baby ", yearjohn );
+
+let numberOne = 0;
+numberOne = false;
+console.log(numberOne);
+numberOne=null;
+console.log(typeof numberOne)
+console.log(numberOne);
+numberOne=undefined;
+//check the data type
+console.log(typeof numberOne);
+console.log(typeof (2**2));
+// concatanate +
+// ** power
+console.log("it's 2022 now, and the ag of John is " + ageJohn, 10**3);
+console.log((3+4 > 10)-2);
+//convert str to num
+console.log(Number('24') + 10);
+
+let resultCal = 24;
+resultCal+=4;
+resultCal++;
+resultCal--;
+console.log(resultCal);
+
+const nm = "James"
+const city = "New York City";
+const borough = "Manhattan";
+const population = 1.632;
+
+console.log("My bestfriend", nm, "lives in", city,
+", and the population of", borough, "is", population);
+//template literal
+console.log(`My bestfriend ${nm} lives in ${city} and 
+the population of ${borough} is ${population}`);
+
+ageJohn=13;
+if ((ageJohn > 16) && (ageJohn <30)){
+    console.log("You can drive");
+} 
+else if (ageJohn > 30){
+    console.log("You should have your driver license");
+}
+else if (ageJohn < 16 && ageJohn > 10){
+    console.log(16-ageJohn)
+}
+else if (ageJohn <= 10 && ageJohn){
+    console.log("You are too young to drive");
+}
+else console.log("You can't drive now");
+
